897e122d92b64f17e6ce1d3e5ad852d18dfcd5b5
Elizabeth Wandia Mugo <wwandia173@gmail.com>
2022-10-02 14:29:11 +0300

    Done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,3 +1,4 @@
+
 class ToysController < ApplicationController
   wrap_parameters format: []
 
@@ -7,18 +8,20 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.invalid.errors.full_messages}, status: :unprocessable_entity
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
   end
 
   def destroy
     toy = Toy.find_by(id: params[:id])
-    toy.destroy
     head :no_content
   end
 
